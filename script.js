f8a5e7ecb09a46e51549fed7acf2720a8827d8c9
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2025-07-06 18:06:32 +0000

    [0.0.1] Fix: chrome scrollbar

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2025 MauriceLambert
+
+  This file is part of HackingPresentationGenerator.
+
+  HackingPresentationGenerator is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  HackingPresentationGenerator is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with HackingPresentationGenerator.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 let slide_index = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
