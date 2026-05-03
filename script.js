d5a854301ad47c66e1ca0467f1689504cb6ea099
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2026-05-03 23:49:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1083,7 +1083,7 @@ const themes = {
     fontFamily: "'Courier New', Courier, monospace",
     transitionSpeed: '0.4s',
   },
-    dsomm1: {
+  dsomm1: {
     dark:  { bgColor: '#303030', textColor: '#e8e8e8' },
     light: { bgColor: '#e8e8e8', textColor: '#303030' },
     fontFamily: "Roboto, Arial, sans-serif",
@@ -1094,6 +1094,18 @@ const themes = {
     light: { bgColor: '#4caf50', textColor: '#303030' },
     fontFamily: "Roboto, Arial, sans-serif",
     transitionSpeed: '0.5s',
+  },
+  manga: {
+    dark:  { bgColor: '#0d0f14', textColor: '#2e6bff' },
+    light: { bgColor: '#9ca3af', textColor: '#ffd000' },
+    fontFamily: "'Bangers', 'Impact', 'Arial Black', sans-serif",
+    transitionSpeed: '0.4s',
+  },
+  webscripts: {
+    dark:  { bgColor: '#222222', textColor: '#dd8a12' },
+    light: { bgColor: '#CCCCCC', textColor: '#27219a' },
+    fontFamily: "Arial, Helvetica, 'Liberation Sans', FreeSans, sans-serif",
+    transitionSpeed: '0.6s',
   }
 };
 
